55826858f3959de61752534b038751aae73b4d1c
Alex Voerman <avoerman89@gmail.com>
2018-03-04 09:46:56 -0500

    changing ui, making percentages

--- boxscore-generator-jq/app.js
+++ boxscore-generator-jq/app.js
@@ -1,50 +1,113 @@
 $(document).ready(function () {
     let globalBoxState = [];
 
-    $('#add').click(function () {
-        let play = {
-            player: $('#player').get(0).value,
-            pts: Number.parseInt($('#pts').val() || 0),
-            reb: Number.parseInt($('#reb').val() || 0),
-            ast: Number.parseInt($('#ast').val() || 0),
-            stl: Number.parseInt($('#stl').val() || 0),
-            blk: Number.parseInt($('#blk').val() || 0)
-        };
+    const defaultPlay = {
+        player: '',
+        fgMake: 0, fgMiss: 0,
+        threeMake: 0, threeMiss: 0,
+        ftMake: 0, ftMiss: 0,
+        reb: 0,
+        ast: 0,
+        stl: 0,
+        blk: 0
+    };
 
-        if (play.player) {
-            addToBoxscore(play);
-            clearForm();
-        }
+    $('#fg-make').click(function () {
+        addToBoxscore({ player: getPlayer(), fgMake: 1 });
     });
 
-    function clearForm() {
-        $('input').each(function (val, ref) {
-            ref.value = '';
-        });
+    $('#fg-miss').click(function () {
+        addToBoxscore({ player: getPlayer(), fgMiss: 1 });
+    });
+
+    $('#three-make').click(function () {
+        addToBoxscore({ player: getPlayer(), threeMake: 1 });
+    });
+
+    $('#three-miss').click(function () {
+        addToBoxscore({ player: getPlayer(), threeMiss: 1 });
+    });
+
+    $('#ft-make').click(function () {
+        addToBoxscore({ player: getPlayer(), ftMake: 1 });
+    });
+
+    $('#ft-miss').click(function () {
+        addToBoxscore({ player: getPlayer(), ftMiss: 1 });
+    });
+
+    $('#reb').click(function () {
+        addToBoxscore({ player: getPlayer(), reb: 1 });
+    });
+
+    $('#ast').click(function () {
+        addToBoxscore({ player: getPlayer(), ast: 1 });
+    });
+
+    $('#stl').click(function () {
+        addToBoxscore({ player: getPlayer(), stl: 1 });
+    });
+
+    $('#blk').click(function () {
+        addToBoxscore({ player: getPlayer(), blk: 1 });
+    });
+
+    function getPlayer() {
+        return $('#player').get(0).value;
     }
 
     function addToBoxscore(play) {
-        let exists = globalBoxState.some(item => item.player === play.player);
+        if (!play.player) {
+            return;
+        }
+
+        let sanitizedPlay = Object.assign({}, defaultPlay, play);
+
+        let exists = globalBoxState.some(item => item.player === sanitizedPlay.player);
+        let boxItem = boxItemFromPlay(sanitizedPlay);
 
         if (!exists) {
-            globalBoxState.push(play);
+            globalBoxState.push(boxItem);
         } else {
             globalBoxState = globalBoxState.map(item => {
-                return play.player === item.player ? total(item, play) : item;
+                return sanitizedPlay.player === item.player ? aggregatePlay(item, boxItem) : item;
             });
         }
 
         renderTable();
     }
 
-    function total(item, play) {
+    function boxItemFromPlay(play) {
+        return {
+            player: play.player,
+            pts: (play.fgMake * 2) + (play.threeMake * 3) + (play.ftMake),
+            fg: play.fgMake,
+            fga: (play.fgMake > 0 || play.fgMiss > 0) ? 1 : 0,
+            three: play.threeMake,
+            threeAtt: (play.threeMake > 0 || play.threeMiss > 0) ? 1 : 0,
+            ft: play.ftMake,
+            fta: (play.ftMake > 0 || play.ftMiss > 0) ? 1 : 0,
+            reb: play.reb,
+            ast: play.ast,
+            stl: play.stl,
+            blk: play.blk
+        };
+    }
+
+    function aggregatePlay(item, newItem) {
         return {
             player: item.player,
-            pts: item.pts + play.pts,
-            reb: item.reb + play.reb,
-            ast: item.ast + play.ast,
-            stl: item.stl + play.stl,
-            blk: item.blk + play.blk
+            pts: item.pts + newItem.pts,
+            fg: item.fg + newItem.fg,
+            fga: item.fga + newItem.fga,
+            three: item.three + newItem.three,
+            threeAtt: item.threeAtt + newItem.threeAtt,
+            ft: item.ft + newItem.ft,
+            fta: item.fta + newItem.fta,
+            reb: item.reb + newItem.reb,
+            ast: item.ast + newItem.ast,
+            stl: item.stl + newItem.stl,
+            blk: item.blk + newItem.blk
         };
     }
 
@@ -59,6 +122,12 @@ $(document).ready(function () {
         let total = $('.total');
         let totalsRecord = totalAll();
         total.find('.pts-total').html(totalsRecord.pts);
+        total.find('.fg-total').html(`${totalsRecord.fg} - ${totalsRecord.fga}`);
+        total.find('.fgpc-total').html(`${percent(totalsRecord.fg, totalsRecord.fga)}%`);
+        total.find('.three-total').html(`${totalsRecord.three} - ${totalsRecord.threeAtt}`);
+        total.find('.threepc-total').html(`${percent(totalsRecord.three, totalsRecord.threeAtt)}%`);
+        total.find('.ft-total').html(`${totalsRecord.ft} - ${totalsRecord.fta}`);
+        total.find('.ftpc-total').html(`${percent(totalsRecord.ft, totalsRecord.fta)}%`);
         total.find('.reb-total').html(totalsRecord.reb);
         total.find('.ast-total').html(totalsRecord.ast);
         total.find('.stl-total').html(totalsRecord.stl);
@@ -69,12 +138,18 @@ $(document).ready(function () {
         return globalBoxState.reduce((prev, cur) => {
                 return {
                     pts: prev.pts + cur.pts,
+                    fg: prev.fg + cur.fg,
+                    fga: prev.fga + cur.fga,
+                    three: prev.three + cur.three,
+                    threeAtt: prev.threeAtt + cur.threeAtt,
+                    ft: prev.ft + cur.ft,
+                    fta: prev.fta + cur.fta,
                     reb: prev.reb + cur.reb,
                     ast: prev.ast + cur.ast,
                     stl: prev.stl + cur.stl,
                     blk: prev.blk + cur.blk
                 };
-            }, { pts: 0, reb: 0, ast: 0, stl: 0, blk: 0 }
+            }, { pts: 0, fg: 0, fga: 0, three: 0, threeAtt: 0, ft: 0, fta: 0, reb: 0, ast: 0, stl: 0, blk: 0 }
         );
     }
 
@@ -82,10 +157,23 @@ $(document).ready(function () {
         return `<tr id="${play.player}">
                         <td>${play.player}</td>
                         <td>${play.pts}</td>
+                        <td>${play.fg} - ${play.fga}</td>
+                        <td>${percent(play.fg, play.fga)}%</td>
+                        <td>${play.three} - ${play.threeAtt}</td>
+                        <td>${percent(play.three, play.threeAtt)}%</td>
+                        <td>${play.ft} - ${play.fta}</td>
+                        <td>${percent(play.ft, play.fta)}%</td>
                         <td>${play.reb}</td>
                         <td>${play.ast}</td>
                         <td>${play.stl}</td>
                         <td>${play.blk}</td>
                     </tr>`;
+    }
+
+    function percent(made = 0, attempts = 0) {
+        if (made === 0) {
+            return 0;
+        }
+        return ((made / attempts) * 100).toFixed(1);
     }
 });
