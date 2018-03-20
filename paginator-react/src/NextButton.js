ae8c4ff3ae70ab61f470258468101397eb9f6c45
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2018-03-20 13:20:52 -0400

    cleaned up paginator stuff

--- paginator-react/src/NextButton.js
+++ paginator-react/src/NextButton.js
@@ -1,6 +1,13 @@
 class NextButton extends React.Component {
+  isDisabled = ({ totalPages, currentPage }) =>
+    totalPages <= 1 || currentPage >= totalPages;
+
   clickHandler = event => {
     event.preventDefault();
+
+    if (this.isDisabled(this.props)) {
+      return;
+    }
 
     let newPage = this.props.currentPage + 1;
     this.props.onClick(newPage);
@@ -13,7 +20,7 @@ class NextButton extends React.Component {
       <a
         className={`button is-info is-rounded`}
         onClick={this.clickHandler}
-        disabled={totalPages <= 1 || currentPage >= totalPages}
+        disabled={this.isDisabled(this.props)}
       >
         <span>Next</span>
         <span className="icon is-small">
--- paginator-react/src/PreviousButton.js
+++ paginator-react/src/PreviousButton.js
@@ -1,6 +1,12 @@
 class PreviousButton extends React.Component {
+  isDisabled = ({ currentPage }) => currentPage <= 1;
+
   clickHandler = event => {
     event.preventDefault();
+
+    if (this.isDisabled(this.props)) {
+      return;
+    }
 
     let newPage = this.props.currentPage - 1;
     this.props.onClick(newPage);
@@ -11,7 +17,7 @@ class PreviousButton extends React.Component {
       <a
         className={`button is-info is-rounded`}
         onClick={this.clickHandler}
-        disabled={this.props.currentPage <= 1}
+        disabled={this.isDisabled(this.props)}
       >
         <span className="icon is-small">
           <i className="fas fa-chevron-left" />
