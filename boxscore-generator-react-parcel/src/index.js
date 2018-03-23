5a0234778a69bbfba051a73eed72be78c74d6314
Alex Voerman <avoerman89@gmail.com>
2018-03-23 08:27:25 -0400

    removing the parcel and webpack examples

--- boxscore-generator-react-parcel/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './assets/index.css';
-
-import App from './App';
-
-const wrapper = document.getElementById('app');
-wrapper && render(<App />, wrapper);
--- boxscore-generator-react-webpack/src/components/BoxscoreGenerator.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BoxscoreGenerator from './BoxscoreGenerator';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<BoxscoreGenerator/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
